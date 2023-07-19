4b87eb7dc2a7e60c416fe71ef058cca547d1a3c2
laterNaT <105680553+laterNaT@users.noreply.github.com>
2023-07-19 17:02:18 +0200

    Add login functionallity

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -3,20 +3,55 @@ import asyncHandler from "express-async-handler";
 import User from "../models/userModel";
 
 const registerUserPost = asyncHandler(async (req: Request, res: Response) => {
-  // first check if the username is already taken
+  // check if already logged in
+  if (req.session.isAuthorized) {
+    res.status(400);
+    throw new Error("Already logged in");
+  }
+
+  // check if the username is already taken
   const user = await User.findOne({ username: req.body.username });
   if (user) {
     res.status(400);
     throw new Error("Username is already taken");
   }
+
   // create the user
   const newUser = await User.create({
     username: req.body.username,
     password: req.body.password,
   });
+
+  // send the response
   res.status(201).json({
     message: "User created successfully",
   });
 });
 
-export { registerUserPost };
+const loginUserPost = asyncHandler(async (req: Request, res: Response) => {
+  // check if already logged in
+  if (req.session.isAuthorized) {
+    res.status(400);
+    throw new Error("Already logged in");
+  }
+
+  // check if the username exists
+  const user = await User.findOne({ username: req.body.username });
+  if (!user) {
+    res.status(400);
+    throw new Error("Username does not exist");
+  }
+  // check if the password is correct
+  if (user.password !== req.body.password) {
+    res.status(400);
+    throw new Error("Password is incorrect");
+  }
+
+  // log the user in
+  req.session.isAuthorized = true;
+  res.status(200).json({
+    message: "User logged in successfully",
+  });
+});
+
+export { loginUserPost, registerUserPost };
--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,7 +1,8 @@
 import express from "express";
-import { registerUserPost } from "../controllers/userController";
+import { loginUserPost, registerUserPost } from "../controllers/userController";
 const router = express.Router();
 
 router.post("/register", registerUserPost);
+router.post("/login", loginUserPost);
 
 export default router;
