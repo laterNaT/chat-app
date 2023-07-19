4d8a2cc5b7ebef997dbd175647ae79b7066ed1d5
laterNaT <105680553+laterNaT@users.noreply.github.com>
2023-07-19 15:48:54 +0200

    Add express-session and compatible mongodb store

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,24 @@
+import MongoStore from "connect-mongo";
 import dotenv from "dotenv";
 import express, { Request, Response } from "express";
+import session from "express-session";
 import mongoose from "mongoose";
 import errorMiddleware from "./middleware/errorMiddleware";
 import userRoutes from "./routes/userRoutes";
-
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 mongoose.connect(process.env.MONGODB_URI!);
-
+app.use(errorMiddleware);
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI! }),
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/users", userRoutes);
@@ -21,5 +30,3 @@ app.get("/", (req: Request, res: Response) => {
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
-
-app.use(errorMiddleware);
