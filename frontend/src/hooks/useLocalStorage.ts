c98460ba7dafe27e22f776433f8651fc7744349f
laterNaT <105680553+laterNaT@users.noreply.github.com>
2023-07-22 21:57:22 +0200

    Add error handling

--- frontend/src/hooks/useLocalStorage.ts
+++ frontend/src/hooks/useLocalStorage.ts
@@ -2,15 +2,23 @@ import { useEffect, useState } from "react";
 
 export function useLocalStorage<T>(key: string) {
   const [value, setValue] = useState<T | null>(() => {
-    const item = localStorage.getItem(key);
-    if (item) {
-      return JSON.parse(item) as T;
+    try {
+      const item = localStorage.getItem(key);
+      if (item) {
+        return JSON.parse(item) as T;
+      }
+    } catch (error) {
+      console.error("Error parsing localStorage with key ", key, error);
     }
     return null;
   });
 
   useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(value));
+    try {
+      localStorage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+      console.error("Error setting localStorage with key ", key, error);
+    }
   }, [key, value]);
 
   return [value, setValue] as const;
