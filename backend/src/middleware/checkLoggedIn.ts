ebe57c1e14f2f60e47d90b56e2dd75a774696077
laterNaT <105680553+laterNaT@users.noreply.github.com>
2023-07-23 17:18:01 +0200

    Add additional checks for DELETE method

--- backend/src/middleware/checkLoggedIn.ts
+++ backend/src/middleware/checkLoggedIn.ts
@@ -1,9 +1,13 @@
 import { NextFunction, Request, Response } from "express";
 
 const checkLoggedIn = (req: Request, res: Response, next: NextFunction) => {
-  if (req.session.isAuthorized) {
+  if (req.session.isAuthorized && req.method !== "DELETE") {
     res.status(400);
     throw new Error("Already logged in");
+  }
+  if (!req.session.isAuthorized && req.method === "DELETE") {
+    res.status(400);
+    throw new Error("Not logged in");
   }
   next();
 };
