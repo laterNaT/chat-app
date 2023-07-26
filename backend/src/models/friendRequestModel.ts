b9d266963281abb3d796528f76623550610eff97
laterNaT <105680553+laterNaT@users.noreply.github.com>
2023-07-26 15:21:47 +0200

    Add timestamps to friend requests

--- backend/src/models/friendRequestModel.ts
+++ backend/src/models/friendRequestModel.ts
@@ -4,25 +4,30 @@ type TFriendRequest = {
   sender: typeof Schema.Types.ObjectId;
   receiver: typeof Schema.Types.ObjectId;
   status: "pending" | "accepted" | "rejected";
+  createdAt: Date;
+  updatedAt: Date;
 };
 
-const friendRequestSchema = new mongoose.Schema<TFriendRequest>({
-  sender: {
-    type: Schema.Types.ObjectId,
-    required: [true, "Please enter a sender"],
-    ref: "User",
+const friendRequestSchema = new mongoose.Schema<TFriendRequest>(
+  {
+    sender: {
+      type: Schema.Types.ObjectId,
+      required: [true, "Please enter a sender"],
+      ref: "User",
+    },
+    receiver: {
+      type: Schema.Types.ObjectId,
+      required: [true, "Please enter a receiver"],
+      ref: "User",
+    },
+    status: {
+      type: String,
+      enum: ["pending", "accepted", "rejected"],
+      default: "pending",
+    },
   },
-  receiver: {
-    type: Schema.Types.ObjectId,
-    required: [true, "Please enter a receiver"],
-    ref: "User",
-  },
-  status: {
-    type: String,
-    enum: ["pending", "accepted", "rejected"],
-    default: "pending",
-  },
-});
+  { timestamps: true },
+);
 
 export default mongoose.model<TFriendRequest>(
   "FriendRequest",
