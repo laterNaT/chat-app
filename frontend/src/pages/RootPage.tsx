0b790f7b3de432e8b6642aa646c980668e05d350
laterNaT <105680553+laterNaT@users.noreply.github.com>
2023-08-03 16:28:52 +0200

    Fix styling on welcome page

--- frontend/src/pages/RootPage.tsx
+++ frontend/src/pages/RootPage.tsx
@@ -1,18 +1,25 @@
-import { Container, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export default function RootPage() {
   return (
-    <Container>
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
       <Row>
-        <h1>Welcome</h1>
-        <p>
-          In this app you chat chat privatly with your friends or speak with
-          strangers in global chat rooms.
-        </p>
-        <Link to="/login" style={{ textDecoration: "none" }}>
-          Continue
-        </Link>
+        <Col xs={12}>
+          <h1>Welcome</h1>
+        </Col>
+        <Col>
+          <p>
+            In this app you chat chat privatly with your friends or speak with
+            strangers in global chat rooms.
+          </p>
+          <Link to="/login" style={{ textDecoration: "none" }}>
+            Continue
+          </Link>
+        </Col>
       </Row>
     </Container>
   );
