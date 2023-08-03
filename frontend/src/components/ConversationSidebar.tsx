49d712735c23459957b93ccebd6dfc57d8720844
laterNaT <105680553+laterNaT@users.noreply.github.com>
2023-08-03 16:46:08 +0200

    Add color to selected conversation in sidebar

--- frontend/src/components/ConversationSidebar.tsx
+++ frontend/src/components/ConversationSidebar.tsx
@@ -1,5 +1,5 @@
 import { Card } from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { TConversations } from "./Sidebar";
 type ConversationProps = TConversations[number];
 
@@ -10,13 +10,25 @@ export default function ConversationSidebar({
   const navigate = useNavigate();
 
   const handleClick = () => {
-    console.log(_id);
     navigate(`/home/conversations/${_id}`);
   };
 
   return (
     <Card className="conversation-sidebar" onClick={handleClick}>
-      <Card.Body>{conversationName}</Card.Body>
+      <Card.Body>
+        <NavLink
+          to={`/home/conversations/${_id}`}
+          className={({ isActive, isPending }) =>
+            isPending
+              ? "text-info"
+              : isActive
+              ? "text-primary"
+              : "text-secondary"
+          }
+        >
+          {conversationName}
+        </NavLink>
+      </Card.Body>
     </Card>
   );
 }
